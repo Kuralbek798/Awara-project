71b358b012dbb2e4ff90fa749f50c0d8022daf5c
admin <agubaev_kuralbek@mail.ru>
2024-12-03 22:05:14 +0500

    1

--- AwaraIT.Training.ConsoleApp/Actions/CalculatePrices2.cs
+++ AwaraIT.Training.ConsoleApp/Actions/CalculatePrices2.cs
@@ -3,9 +3,10 @@ using AwaraIT.Training.Domain.Models.Crm.SystemEntities;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
 using System;
+using System.Activities;
 using System.Linq;
-using System.Web.Services.Description;
 
 namespace AwaraIT.Kuralbek.Plugins.Actions
 {
@@ -17,8 +18,9 @@ namespace AwaraIT.Kuralbek.Plugins.Actions
             {
                 using (var client = Program.GetCrmClient())
                 {
-                    var clietntD365 = (IOrganizationService)client;
-                    var tsPl = new Test(clietntD365);
+                    var clientD365 = (IOrganizationService)client;
+                    var test = new Test(clientD365);
+                    test.Execute();
                 }
             }
             catch (Exception e)
@@ -28,20 +30,40 @@ namespace AwaraIT.Kuralbek.Plugins.Actions
         }
     }
 
-    public class Test
+    public class Test : CodeActivity
     {
+        private CodeActivityContext context;
+
+        [Output("BasePrice")]
+        public OutArgument<Money> BasePrice { get; set; }
+
+        [Output("DiscountedPrice")]
+        public OutArgument<Money> DiscountedPrice { get; set; }
+
         IOrganizationService _service;
 
         public Test(IOrganizationService service)
         {
             _service = service;
+        }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.context = context;
             Execute();
         }
 
         public void Execute()
         {
+            /*   var workflowContext = context.GetExtension<IWorkflowContext>();
+               var serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+               var service = serviceFactory.CreateOrganizationService(workflowContext.UserId);*/
+
+
             try
             {
+                Console.WriteLine("Starting Execute method...");
+
                 var productCart = _service.Retrieve(ProductCart.EntityLogicalName, Guid.Parse("9bbbdbcb-84b1-ef11-b8e9-000d3a5c09a6"), new ColumnSet(true)).ToEntity<ProductCart>();
                 var possibleDealRef = productCart.PossibleDealReference;
                 var productRef = productCart.ProductReference;
@@ -53,27 +75,22 @@ namespace AwaraIT.Kuralbek.Plugins.Actions
                     return;
                 }
 
-
-
                 var possibleDeal = _service.Retrieve(possibleDealRef.LogicalName, possibleDealRef.Id,
                                             new ColumnSet(PossibleDeal.Metadata.TerritoryReference)).ToEntity<PossibleDeal>();
-                var territoryReference = possibleDeal.TerritoryReference;
 
+                var territoryReference = possibleDeal.TerritoryReference;
 
                 if (territoryReference == null || territoryReference.Id == Guid.Empty)
                 {
-
                     throw new InvalidPluginExecutionException("Failed to retrieve territory from possible deal");
                 }
 
-                // Получаем информацию о продукте
                 var productEntity = _service.Retrieve(productRef.LogicalName, productRef.Id,
                                                      new ColumnSet(Product.Metadata.FormatPreparationReference,
                                                                    Product.Metadata.FormatConductionReference,
                                                                    Product.Metadata.SubjectPreparationReference)).ToEntity<Product>();
                 if (productEntity == null)
                 {
-
                     throw new InvalidPluginExecutionException("Product info is null");
                 }
 
@@ -81,56 +98,18 @@ namespace AwaraIT.Kuralbek.Plugins.Actions
                 var formatConducting = productEntity.FormatConductionReference.Id;
                 var subjectPreparation = productEntity.SubjectPreparationReference.Id;
 
-                QueryExpression quer1 = new QueryExpression(PriceListPositions.EntityLogicalName)
-                {
-                    ColumnSet = new ColumnSet(true),
-                    Criteria = new FilterExpression
-                    {
-                        Conditions =
-                         {
-                             new ConditionExpression(PriceListPositions.Metadata.PriceListPostionId, ConditionOperator.Equal, Guid.Parse("b66a97f3-83b1-ef11-b8e9-000d3a5c09a6")),
-
-                         }
-                    }
-                };
-
-                var priceListPositions = _service.RetrieveMultiple(quer1).Entities.FirstOrDefault().ToEntity<PriceListPositions>();
-                var price = priceListPositions.Price.Value.ToString();
-                var territoryRef = priceListPositions.TerritoryReference.Id.ToString();
-                var priceListRef = priceListPositions.PriceListReference.Id.ToString();
-                var formatPreparationRef = priceListPositions.FormatPreparationReference.Id.ToString();
-                var formatConductionRef = priceListPositions.FormatConductionReference.Id.ToString();
-                var subjectRef = priceListPositions.SubjectReference.Id.ToString();
-                Console.WriteLine($"priceListRef {priceListRef}");
-                Console.WriteLine($"price {price}");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine($"territoryRef {territoryRef}");
-                Console.WriteLine($"territoryReference.Id.ToString()}} {territoryReference.Id.ToString()}");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine($"formatPreparationRef {formatPreparationRef}");
-                Console.WriteLine($"formatPreparation.ToString() {formatPreparation.ToString()}");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine($"formatConductionRef {formatConductionRef}");
-                Console.WriteLine($"formatConducting.ToString() {formatConducting.ToString()}");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine($"subjectRef {subjectRef}");
-                Console.WriteLine($"subjectPreparation.ToString() {subjectPreparation.ToString()}");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-
-
-                // Запрос к прайс листу для получения базовой цены
                 QueryExpression query = new QueryExpression(PriceListPositions.EntityLogicalName)
                 {
                     ColumnSet = new ColumnSet(true),
                     Criteria = new FilterExpression
                     {
                         Conditions =
-                         {
-                             new ConditionExpression(PriceListPositions.Metadata.TerritoryReference, ConditionOperator.Equal, territoryReference.Id),
-                             new ConditionExpression(PriceListPositions.Metadata.FormatPreparationReference, ConditionOperator.Equal, formatPreparation),
-                             new ConditionExpression(PriceListPositions.Metadata.FormatConductionReference, ConditionOperator.Equal, formatConducting),
-                             new ConditionExpression(PriceListPositions.Metadata.SubjectReference, ConditionOperator.Equal, subjectPreparation)
-                         }
+                             {
+                                 new ConditionExpression(PriceListPositions.Metadata.TerritoryReference, ConditionOperator.Equal, territoryReference.Id),
+                                 new ConditionExpression(PriceListPositions.Metadata.FormatPreparationReference, ConditionOperator.Equal, formatPreparation),
+                                 new ConditionExpression(PriceListPositions.Metadata.FormatConductionReference, ConditionOperator.Equal, formatConducting),
+                                 new ConditionExpression(PriceListPositions.Metadata.SubjectReference, ConditionOperator.Equal, subjectPreparation)
+                             }
                     }
                 };
 
@@ -140,126 +119,22 @@ namespace AwaraIT.Kuralbek.Plugins.Actions
                     throw new InvalidPluginExecutionException("No data in price list.");
                 }
 
-                var priceListPositionId = result.PriceListPostionId;
-                var priceFinal = result.Price;
-                var preceListReferen = result.PriceListReference;
-                var territoryReferen = result.TerritoryReference;
-                var formatPreprationRef = result.FormatPreparationReference;
-                var formatConductRef = result.FormatConductionReference;
-                var subjectRef1 = result.SubjectReference;
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
-                Console.WriteLine($"priceListPositionId {priceListPositionId}");
-                Console.WriteLine($"priceFinal {priceFinal.Value.ToString()}");
-                Console.WriteLine($"preceListReferen {preceListReferen.Id}");
-                Console.WriteLine($"territoryReferen {territoryReferen.Id}");
-                Console.WriteLine($"formatPreprationRef {formatPreprationRef.Id}");
-                Console.WriteLine($"formatConductRef {formatConductRef.Id}");
-                Console.WriteLine($"subjectRef1 {subjectRef1.Id}");
+                Money basePrice = result.Price;
+                Money discountedPrice = new Money(basePrice.Value - discount.Value);
 
+                Console.WriteLine($"BasePrice: {basePrice.Value}");
+                Console.WriteLine($"DiscountedPrice: {discountedPrice.Value}");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                /*var req = new OrganizationRequest("fnt_CalculatePricingForPossibleDeal");
-                req.Parameters.Add("PossibleDealId", possibleDealRef);
-                req.Parameters.Add("ProductId", productRef);
-                req.Parameters.Add("Discount", discount);
-                var resp = _service.Execute(req);*/
-
-                // Console.WriteLine($"Response: {resp.ToString()}");
+                if (context != null)
+                {
+                    BasePrice.Set(context, basePrice);
+                    DiscountedPrice.Set(context, discountedPrice);
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Exception: {ex.Message}");
             }
         }
-
-
-
-        /*   public void Execute()
-         {
-             var productReferenceId = Guid.Parse("ba505fa4-a7ac-ef11-b8e9-000d3a5c09a6");
-
-             QueryExpression query = new QueryExpression(PosibleDeal.EntityLogicalName)
-             {
-                 ColumnSet = new ColumnSet(PosibleDeal.Metadata.TerritoryReference),
-                 LinkEntities =
-                 {
-                     new LinkEntity(PosibleDeal.EntityLogicalName, PossibleDealProductCartNN.EntityLogicalName, PosibleDeal.Metadata.PosibleDealId, PossibleDealProductCartNN.Metadata.PossibleDealId, JoinOperator.Inner)
-                     {
-                         LinkEntities =
-                         {
-                             new LinkEntity(PossibleDealProductCartNN.EntityLogicalName, ProductCart.EntityLogicalName, PossibleDealProductCartNN.Metadata.ProductCartId, ProductCart.Metadata.ProductCartId, JoinOperator.Inner)
-                             {
-                                 Columns = new ColumnSet(ProductCart.Metadata.ProductReference, ProductCart.Metadata.Price, ProductCart.Metadata.Discount),
-                                 EntityAlias = "productCart",
-                                 LinkCriteria = new FilterExpression
-                                 {
-                                     Conditions =
-                                     {
-                                         new ConditionExpression(ProductCart.Metadata.ProductCartId, ConditionOperator.Equal, productReferenceId)
-                                     }
-                                 }
-                             }
-                         }
-                     }
-                 },
-                 Criteria = new FilterExpression
-                 {
-                     Conditions =
-                     {
-                         new ConditionExpression(PosibleDeal.Metadata.PosibleDealId, ConditionOperator.Equal, productReferenceId)
-                     }
-                 }
-             };
-
-             var result = _service.RetrieveMultiple(query).Entities.FirstOrDefault();
-
-             if (result != null)
-             {
-                 var dealId = result.GetAttributeValue<Guid>(PosibleDeal.Metadata.PosibleDealId);
-                 var territory = result.GetAttributeValue<EntityReference>(PosibleDeal.Metadata.TerritoryReference);
-                 var productCartId = result.GetAttributeValue<AliasedValue>("productCart." + ProductCart.Metadata.ProductCartId)?.Value as Guid?;
-                 var productFormatPreparation = result.GetAttributeValue<AliasedValue>("productCart." + ProductCart.Metadata.ProductReference)?.Value as string;
-                 var productFormatConduction = result.GetAttributeValue<AliasedValue>("productCart." + ProductCart.Metadata.Price)?.Value as Money;
-                 var productSubjectPreparation = result.GetAttributeValue<AliasedValue>("productCart." + ProductCart.Metadata.Discount)?.Value as Money;
-
-                 Console.WriteLine($"DealId: {dealId}");
-                 Console.WriteLine($"Territory: {territory?.Id}");
-                 Console.WriteLine($"ProductCartId: {productCartId}");
-                 Console.WriteLine($"ProductFormatPreparation: {productFormatPreparation}");
-                 Console.WriteLine($"ProductFormatConduction: {productFormatConduction}");
-                 Console.WriteLine($"ProductSubjectPreparation: {productSubjectPreparation}");
-             }
-             else
-             {
-                 Console.WriteLine("No matching records found.");
-             }
-         }*/
     }
 }
-
